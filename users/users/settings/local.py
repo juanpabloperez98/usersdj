a220efb56ba8613d4c74a3dde4c497bc6e7edd50
Juan pablo perez <juanpablo.perez@utp.edu.co>
2022-03-28 16:17:41 -0500

    Adding requirements and modifing settings files

---
+++ users/users/settings/local.py
@@ -0,0 +1,37 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_users',
+        'USER': 'postgres',
+        'PASSWORD':'Juanpablo7823',
+        'HOST':'localhost',
+        'PORT':'5432'
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR.parent,"media")
